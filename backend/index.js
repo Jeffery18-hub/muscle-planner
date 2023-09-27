6fadd2afb2451a1edcc4f2a6a881091919320fef
Jeffery18-hub <ljzmcs@gmail.com>
2023-09-27 13:20:32 -0600

    deploy try 1

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,27 @@ const express = require('express');
 const cors = require('cors');
 const authRouter = require('./routes/authRoute');
 const youtubeRouter = require('./routes/youtubeRoute');
-const bodyParser = require('body-parser');
 const app = express();
-const port = 3001;
+const port = 5001;
+const path  = require('path')
+
+const _dirname = path.dirname("")
+const buildPath = path.join(_dirname  , "../frontend/build");
+
+app.use(express.static(buildPath))
+
+app.get("/*", function(req, res){
+
+    res.sendFile(
+        path.join(__dirname, "../frontend/build/index.html"),
+        function (err) {
+          if (err) {
+            res.status(500).send(err);
+          }
+        }
+      );
+
+})
 
 app.use(cors());
 app.use(express.json());
